2ea7dd2780c936bce6487d0dca44bbe4fff78edf
Galen Clark Haynes <gchaynes@gmail.com>
2014-11-15 20:00:18 -0500

    added serialization to XML-RPC values, library heavily based upon Rust json serialization

---
+++ Makefile
@@ -0,0 +1,11 @@
+all:
+	cargo build
+
+test:
+	cargo test
+
+clean:
+	cargo clean
+
+example:
+	cargo run --example simple
---
+++ examples/simple.rs
@@ -0,0 +1,43 @@
+extern crate xmlrpc;
+extern crate serialize;
+use serialize::Encodable;
+use serialize::json;
+
+fn main() {
+
+    println!("XML of string: {}", xmlrpc::encode(&"hello world!".to_string()));
+    println!("XML of char: {}", xmlrpc::encode(&'c'));
+    println!("XML of int: {}", xmlrpc::encode(&9248i));
+    println!("XML of float: {}", xmlrpc::encode(&19824.28824f32));
+    println!("XML of bool (true): {}", xmlrpc::encode(&true));
+    println!("XML of bool (false): {}", xmlrpc::encode(&false));
+
+    #[deriving(Encodable)]
+    struct MyStruct {
+        value: String
+    }
+    let m: MyStruct = MyStruct { value: "foobar".to_string() };
+    println!("XML of struct: {}", xmlrpc::encode(&m));
+
+    let v = vec![1i,2i,3i,4i];
+    println!("XML of int vector: {}", xmlrpc::encode(&v));
+
+    #[deriving(Encodable)]
+    struct City {
+        name: &'static str,
+        // Latitude
+        lat: f32,
+        // Longitude
+        lon: f32,
+    }
+
+    for city in [
+        City { name: "São Paulo", lat: -23.55,     lon: -46.633333 },
+        City { name: "Lima",      lat: -12.043333, lon: -77.028333 },
+        City { name: "Santiago",  lat: -33.45,     lon: -70.666667 },
+    ].iter() {
+        // `encode` encodes an `Encodable` implementor into a `String`
+        println!("JSON: {}", json::encode(city));
+        println!("XML: {}", xmlrpc::encode(city));
+    }
+}
