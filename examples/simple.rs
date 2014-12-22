5338d3ae8d57895fb1839ee882e22f6480272388
Galen Clark Haynes <gchaynes@gmail.com>
2014-12-22 09:37:26 -0500

    added methods (still based upon json.rs) for introspection and grabbing of Xml data types.  Not yet moved onto

--- examples/simple.rs
+++ examples/simple.rs
@@ -22,8 +22,31 @@ fn main() {
     let v = vec![1i,2i,3i,4i];
     println!("XML of int vector: {}", xmlrpc::encode(&v));
 
-    println!("XML of string Xml object: {}", xmlrpc::encode(&xmlrpc::Xml::I64(32i64)));
+    println!("XML of string Xml object: {}", xmlrpc::encode(&xmlrpc::Xml::I32(32)));
     println!("XML of string Xml object: {}", xmlrpc::encode(&xmlrpc::Xml::Boolean(false)));
+
+    let v = vec![xmlrpc::Xml::I32(31),
+                 xmlrpc::Xml::Boolean(true),
+                 xmlrpc::Xml::String("hello world!".to_string())];
+    println!("XML of xml vector: {}", xmlrpc::encode(&v));
+    println!("JSON of same xml vector: {}", json::encode(&v));
+
+    // grab a string value out of an xml value
+    println!("Is index 0 a string? {}", v[1].is_string());
+    println!("Is index 1 a string? {}", v[1].is_string());
+    println!("Is index 2 a string? {}", v[2].is_string());
+    println!("Index 2 as a string: {}", v[2].as_string().unwrap());
+
+    // make an Xml object of an arbitrary struct
+    // FIXME: is there a way to automatically translate this into BTreeMap?
+    //#[deriving(Encodable)]
+    //struct Person {
+    //    name: &'static str,
+    //    age: i32
+    //}
+    //let p = Person { name: "Clark", age: 35 };
+    //println!("XML of person: {}", xmlrpc::encode(&xmlrpc::Xml::Object(p)));
+
 
     #[deriving(Encodable)]
     struct City {
@@ -34,13 +57,14 @@ fn main() {
         lon: f32,
     }
 
-    for city in [
-        City { name: "São Paulo", lat: -23.55,     lon: -46.633333 },
-        City { name: "Lima",      lat: -12.043333, lon: -77.028333 },
-        City { name: "Santiago",  lat: -33.45,     lon: -70.666667 },
-    ].iter() {
-        // `encode` encodes an `Encodable` implementor into a `String`
-        println!("JSON: {}", json::encode(city));
-        println!("XML: {}", xmlrpc::encode(city));
-    }
+    // FIXME: iters not working right now??
+    // for city in [
+    //     City { name: "São Paulo", lat: -23.55,     lon: -46.633333 },
+    //     City { name: "Lima",      lat: -12.043333, lon: -77.028333 },
+    //     City { name: "Santiago",  lat: -33.45,     lon: -70.666667 },
+    // ].iter() {
+    //     // `encode` encodes an `Encodable` implementor into a `String`
+    //     println!("JSON: {}", json::encode(city));
+    //     println!("XML: {}", xmlrpc::encode(city));
+    // }
 }
